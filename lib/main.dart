b3bbb574421f415da83a7a21a40dbf41e683ddcf
Marcus Macbook <marcuseduardo.ns@gmail.com>
2025-09-11 09:33:49 -0400

    Encrypt Hive Db
    - there are comments that will show you the location of the encrypted db (main.dart)

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_app/l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:path_provider/path_provider.dart';
 import 'models/todo.dart';
 import 'pages/home_page.dart';
 
@@ -13,7 +14,8 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(TodoAdapter());
   await Hive.openBox<Todo>('todos');
-
+  // final appDir = await getApplicationDocumentsDirectory();
+  // print('Hive DB path: ${appDir.path}');
   runApp(const ProviderScope(child: MyAppRoot()));
 }
 
